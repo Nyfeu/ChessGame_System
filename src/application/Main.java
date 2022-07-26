c4128e963a2dfb2a4cb6d9e0cdec256c37aebe55
André Maiolini <andre.maiolini@hotmail.com>
2022-07-26 00:22:45 -0300

    Implementando a classe ChessMatch (partida de xadrez) e a interface de usuário - classe UI.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,13 @@
 package application;
 
+import chess_layer.ChessMatch;
+
 public class Main {
 
     public static void main(String[] args) {
 
-
+        ChessMatch match = new ChessMatch();
+        UI.printBoard(match.getPieces());
 
     }
 
