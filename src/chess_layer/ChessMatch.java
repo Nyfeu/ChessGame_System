e772ba3d72a974b59931d7bd96985b27dbb9abe0
André Maiolini <andre.maiolini@hotmail.com>
2022-07-26 01:10:37 -0300

    Colorindo a interface de usuário!

--- src/chess_layer/ChessMatch.java
+++ src/chess_layer/ChessMatch.java
@@ -2,6 +2,7 @@ package chess_layer;
 
 import board_layer.Board;
 import chess_layer.enums.Color;
+import chess_layer.pieces.King;
 import chess_layer.pieces.Rook;
 
 public class ChessMatch {
@@ -34,7 +35,19 @@ public class ChessMatch {
     }
 
     private void initialSetup() {
-        placeNewPiece('b',6,new Rook(board,Color.WHITE));
+        placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new King(board, Color.WHITE));
+
+        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 8, new King(board, Color.BLACK));
     }
 
 }
